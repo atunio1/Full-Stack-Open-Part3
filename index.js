ab7932247be4854b576132c2fadd32733c8c87d3
atunio1 <atunio@seas.upenn.edu>
2024-06-21 15:07:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request,response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  // error handling
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  const duplicate = persons.find(({ name }) => name.toLowerCase() === (body.name).toLowerCase()) 
+
+  if (duplicate) {
+    return response.status(400).json({ 
+      error: 'name already exists in phonebook' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: getRandomInt(1,10000),
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const getRandomInt = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
