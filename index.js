143d5364f939d7f253ffab9d4a39b8b5b145128f
atunio1 <atunio@seas.upenn.edu>
2024-06-26 12:46:23 -0400

    copied production build from part2 to backend repo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-
+app.use(express.static('dist'))
 
 morgan.token('content-data', function getContent (req) {
   return JSON.stringify(req.body)
